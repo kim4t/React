1d265269a0373d39dd4f8019edc0e788faad7a67
Your Name <you@example.com>
2023-03-28 08:48:28 -0500

    css module

--- react-for-beginner/src/App.js
+++ react-for-beginner/src/App.js
@@ -1,7 +1,11 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useState } from "react";
 function App() {
   return (
     <div>
-      <h1>Welcome Back!</h1>
+      <h1 classame={styles.title}>Welcome Back!</h1>
+      <Button text={"Continue"} />
     </div>
   );
 }
---
+++ react-for-beginner/src/Button.js
@@ -0,0 +1,10 @@
+import Proptypes from "prop-types";
+import styles from "./Button.module.css";
+function Button({ text }) {
+  return <button className={styles.btn}>{text}</button>;
+}
+
+Button.propTypes = {
+  text: Proptypes.string.isRequired,
+};
+export default Button;
